29972390d80aabe27fa1f8c598e096e301f107b2
trdeveloper22 <trdeveloper22@gmail.com>
2024-09-04 11:35:48 -0300

    recuperacion

--- resources/views/client/report.blade.php
+++ resources/views/client/report.blade.php
@@ -1,11 +1,43 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+    <style>
+        /* Control del espaciado entre líneas */
+        body,
+        p {
+            line-height: 1.2;
+        }
+
+        /* Reducción de márgenes entre elementos */
+        h5,
+        h6 {
+            margin-top: 5px;
+            margin-bottom: 5px;
+        }
+
+        p {
+            margin-top: 3px;
+            margin-bottom: 3px;
+        }
+
+        table {
+            margin-top: 10px;
+            margin-bottom: 10px;
+        }
+
+        /* Ajuste del padding en celdas de tabla */
+        td,
+        th {
+            padding: 5px !important;
+        }
+    </style>
     <title>Reporte General de Deudores</title>
 </head>
+
 <body>
     <div class="text-center">
         <img src="vendor/adminlte/dist/img/logo.png" width="100px">
@@ -13,18 +45,13 @@
         <p>Saldo Total: <strong>{{ $total }}</strong></p>
         <hr>
         @foreach($clients as $client)
-            <table class="table text-center">
+        <div class="table-responsive-sm">
+            <table class="table table-bordered">
                 <thead>
-                    <tr>
-                        <td>
-                            <p>Cliente</p>
-                        </td>
-                        <td>
-                            <p>DNI/CUIT</p>
-                        </td>
-                        <td>
-                            <p>Saldo</p>
-                        </td>
+                    <tr class="table-info">
+                        <th scope="col">Cliente</th>
+                        <th scope="col">DNI/CUIT</th>
+                        <th scope="col">SALDO</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -36,28 +63,36 @@
                 </tbody>
             </table>
             <h6>Facturas</h6>
-                <table class="table table-bordered text-center">
-                    <thead>
-                        <th>Numero</th>
-                        <th>Fecha</th>
-                        <th>Total</th>
-                    </thead>
-                    <tbody>
-                            @foreach($client->invoices as $invoice)
-                                @if($invoice->paid == 'NO')
-                                <tr>
-                                    <td>{{ $invoice->id }}</td>
-                                    <td>{{ $invoice->date }}</td>
-                                    <td>{{ $invoice->totalWithIva }}</td>
-                                </tr>
-                                @endif
-                            @endforeach
-                            </tbody>
-                        </table>
-        @endforeach
+            <table class="table table-bordered">
+                <thead>
+                    <tr class="table-secondary">
+                        <th scope="col">#</th>
+                        <th scope="col">Fecha</th>
+                        <th scope="col">Vencimiento</th>
+                        <th scope="col">Total</th>
+                    </tr>
+                </thead>
+                <tbody>
+                <tbody>
+                    @foreach($client->invoices as $invoice)
+                    @if($invoice->paid == 'NO')
+                    <tr>
+                        <td>{{ $invoice->id }}</td>
+                        <td>{{ $invoice->date }}</td>
+                        <td>{{ \Carbon\Carbon::parse($invoice->date)->addDays(30)->format('Y-m-d') }}</td>
+                        <td>{{ $invoice->totalWithIva }}</td>
+                    </tr>
+                    @endif
+                    @endforeach
+                </tbody>
+                </tbody>
+            </table>
+            @endforeach
+        </div>
         <p>Listado de cuentas corrientes al {{ $date }}</p>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
 </body>
+
 </html>
--- resources/views/invoice/pdf.blade.php
+++ resources/views/invoice/pdf.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -12,14 +13,15 @@
         padding: 65px;
     }
 </style>
+
 <body>
     <div class="container text-center">
         <div class="col-12">
             <h5>FACTURA N°{{ $invoice->number }}</h5>
         </div>
-            <div class="row ">
-                <img class="col-7" src="https://media.licdn.com/dms/image/C4D1BAQF9AP8K9M-0WQ/company-background_10000/0/1625358131993/transportes_ruta_s_r_l_cover?e=2147483647&v=beta&t=DMcRvoePh7phfXc3qOGVvqPwkBOIDx37opmL1OcJizM">
-            </div>
+        <div class="row ">
+            <img class="col-7" src="https://media.licdn.com/dms/image/C4D1BAQF9AP8K9M-0WQ/company-background_10000/0/1625358131993/transportes_ruta_s_r_l_cover?e=2147483647&v=beta&t=DMcRvoePh7phfXc3qOGVvqPwkBOIDx37opmL1OcJizM">
+        </div>
 
 
         <div class="col-12 table-bordered text-left">
@@ -31,23 +33,37 @@
         </div>
         <div class="col-12 table-bordered text-left">
             <p><strong>CONCEPTOS:</strong></p>
-            @foreach($invoice->travelCertificates as $travelCertificate)
-                Constancia N°{{ $travelCertificate->number }} con un total de ${{ $travelCertificate->total + $travelCertificate->iva }}
-            @endforeach            
-        </div>
-        <div class="col-12 table-bordered text-left">
-            <p><strong>TOTAL:</strong> ${{ $invoice->total }}</p>
-            <p><strong>IVA:</strong> ${{ $invoice->iva }}</p>
-            <p><strong>TOTAL CON IVA:</strong> ${{ $invoice->totalWithIva }}</p>
-        </div>
+            <table border="1" cellspacing="0" style="border-collapse: collapse;">
+                <thead>
+                    <tr>
+                        <th style="padding: 2px;">Constancia N°</th>
+                        <th style="padding: 2px;">Total</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach($invoice->travelCertificates as $travelCertificate)
+                    <tr>
+                        <td style="padding: 2px;">{{ $travelCertificate->number }}</td>
+                        <td style="padding: 2px;">${{ $travelCertificate->total + $travelCertificate->iva }}</td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
+            <br>
+            <div class="col-12 table-bordered text-left">
+                <p><strong>TOTAL:</strong> ${{ $invoice->total }}</p>
+                <p><strong>IVA:</strong> ${{ $invoice->iva }}</p>
+                <p><strong>TOTAL CON IVA:</strong> ${{ $invoice->totalWithIva }}</p>
+            </div>
             <div class="col-12 table-bordered text-center">
                 <br>
                 <br>
                 <hr class="col-6">
                 <p>FIRMA</p>
             </div>
-    </div>
-    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
+        </div>
+        <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
 </body>
+
 </html>
