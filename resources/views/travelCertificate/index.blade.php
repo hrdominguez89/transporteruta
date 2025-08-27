4aa1b25e4b9db00e5e69981791f6a6bd8864a667
Lautaro Marguery <l.e.marguery@gmail.com>
2025-08-27 02:03:57 -0300

    Se valida la constancia repetida

--- resources/views/travelCertificate/index.blade.php
+++ resources/views/travelCertificate/index.blade.php
@@ -11,6 +11,17 @@
 @stop
 
 @section('content')
+    @if (session('success'))
+        <div class="alert alert-success">
+            {{ session('success') }}
+        </div>
+    @endif
+    
+    @if (session('warning'))
+        <div class="alert alert-warning">
+            {{ session('warning') }}
+        </div>
+    @endif
     <table class="table table-sm table-bordered text-center data-table">
         <thead class="bg-danger">
             <tr>
