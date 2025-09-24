091a086c6d51602841056e6542335ba5b5554361
Lautaro Marguery <l.e.marguery@gmail.com>
2025-09-24 12:19:51 -0300

    Se corrio un dia la fecha de busqueda de constancias de viaje en la liquidacion

--- resources/views/driverSettlement/show.blade.php
+++ resources/views/driverSettlement/show.blade.php
@@ -217,7 +217,7 @@
                 </thead>
                 <tbody>
                     @foreach ($driverSettlement->driver->travelCertificates as $travelCertificate)
-                        @if ($travelCertificate->date >= $driverSettlement->dateFrom and $travelCertificate->date <= $driverSettlement->dateTo)
+                        @if ($travelCertificate->date >= $driverSettlement->dateFrom and $travelCertificate->date <= \Carbon\Carbon::parse($driverSettlement->dateTo)->addDay())
                             @if ($travelCertificate->driverSettlementId != $driverSettlement->id)
                                 <tr>
                                     <td style="font-size:14px;" class="text-center"
