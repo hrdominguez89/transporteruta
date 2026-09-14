8f1ab38e00aaee79b5fc293860ad080f179ff0fd
ARESLautaro95estudio <l.e.marguery@gmail.com>
2026-09-14 11:59:35 -0300

    se modifico la tabla inicial para la vista del cliente habilitando el valor declarado y el precio de la carga

--- resources/views/stock/client/index.blade.php
+++ resources/views/stock/client/index.blade.php
@@ -64,6 +64,8 @@
                 <th>Pallets normales</th>
                 <th>Pallets grandes</th>
                 <th>Estado de envio</th>
+                <th>Costo por remito</th>
+                <th>Valor declarado</th>
                 <th>Acciones</th>
             </tr>
         </thead>
@@ -87,6 +89,8 @@
                     <td>{{ $carga->cantidad_bulto }}</td>
                     <td>{{ $carga->cantidad_pallet_normal }}</td>
                     <td>{{ $carga->cantidad_pallet_grande }}</td>
+                    <td>{{ $carga->precio }}</td>
+                    <td>{{ $carga->remito?->valor_declarado }}</td>
                     <td>{{ $carga->estadoActual?->estado }}</span>  {{ $carga->estadoActual?->horario }} </td>
                     <td>
                         <a href="{{ Route('showcarga', $carga->id) }}" class="btn btn-sm btn-info">Ver</a>
